8c6f00542fc3856b6c671b4008d6ac2d827a3ef8
Emily Sellers <emilymelindasellers@gmail.com>
2022-11-03 11:27:04 -0700

    add trees route test, failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,12 +1,27 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const { trees } = require('../lib/trees-data');
+
+describe('trees routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('/trees should return a list of trees', async () => {
+    const res = await request(app).get('/trees');
+    const expected = trees.map((tree) => {
+      return { id: tree.id, name: tree.name };
+    });
+    expect(res.body).toEqual(expected);
+  });
+
+  // describe('backend-express-template routes', () => {
+  //   beforeEach(() => {
+  //     return setup(pool);
+  //   });
   // it('example test - delete me!', () => {
   //   expect(1).toEqual(1);
   // });
